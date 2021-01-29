5da9c217a8f47ce9b4daf83beb7d51cb1e7624d8
Vex87 <AndrewKyleGuilas@gmail.com>
2021-01-28 22:03:53 -0500

    removed govee.py, end of project

--- govee.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-API_KEY = '0915aca2-be7d-4838-9197-1f4440bf7b59'
-URL = 'https://developer-api.govee.com/v1/devices'
-
-headers = {'Govee-API-Key': '{key}'.format(key = API_KEY)}
-response = requests.get(URL, headers = headers).json()
-
-print(response)
